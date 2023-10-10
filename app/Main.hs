af9b43530e3e6e28beffc46bc9ba1c70bd61741b
konnik <niklas.konstenius@gmail.com>
2023-10-10 20:54:08 +0200

    reformat

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,30 @@
 module Main where
 
 import System.Environment (getArgs)
-import System.IO (Handle, IOMode (..), hGetContents, hPutStrLn, openFile, stderr, stdin)
+import System.IO (Handle, IOMode (ReadMode), hClose, hGetContents, hPutStrLn, openFile, stderr, stdin)
 
 import Json qualified
 import System.Exit (exitFailure)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [] ->
-      parseJsonFromHandle stdin
-    [filename] ->
-      parseJsonFromHandle =<< openFile filename ReadMode
-    _ -> do
-      hPutStrLn stderr "Wrong number of arguments."
-      exitFailure
+    args <- getArgs
+    case args of
+        [] ->
+            parseJsonFromHandle stdin
+        [filename] -> do
+            h <- openFile filename ReadMode
+            parseJsonFromHandle h
+            hClose h
+        _ -> do
+            hPutStrLn stderr "Wrong number of arguments."
+            exitFailure
 
 parseJsonFromHandle :: Handle -> IO ()
 parseJsonFromHandle fileHandle = do
-  result <- Json.parse <$> hGetContents fileHandle
-
-  case result of
-    Just json -> do
-      putStrLn "Parsed JSON: "
-      print json
-    _ -> hPutStrLn stderr "Parse failure."
+    result <- Json.parse <$> hGetContents fileHandle
+    case result of
+        Just json -> do
+            putStrLn "Parsed JSON: "
+            print json
+        _ -> hPutStrLn stderr "Parse failure."
