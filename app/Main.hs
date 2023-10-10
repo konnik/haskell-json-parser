4c88491eeb00cc579d76c4039c7c276cd95ba769
konnik <niklas.konstenius@gmail.com>
2023-10-10 20:46:59 +0200

    make a simple app

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,29 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.IO (Handle, IOMode (..), hGetContents, hPutStrLn, openFile, stderr, stdin)
+
+import Json qualified
+import System.Exit (exitFailure)
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  args <- getArgs
+  case args of
+    [] ->
+      parseJsonFromHandle stdin
+    [filename] ->
+      parseJsonFromHandle =<< openFile filename ReadMode
+    _ -> do
+      hPutStrLn stderr "Wrong number of arguments."
+      exitFailure
+
+parseJsonFromHandle :: Handle -> IO ()
+parseJsonFromHandle fileHandle = do
+  result <- Json.parse <$> hGetContents fileHandle
+
+  case result of
+    Just json -> do
+      putStrLn "Parsed JSON: "
+      print json
+    _ -> hPutStrLn stderr "Parse failure."
